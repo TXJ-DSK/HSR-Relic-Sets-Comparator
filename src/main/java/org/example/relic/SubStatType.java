f698c47c1328e8bc695be9c5ae7cf376f3898521
TXJ-DSK <TXJ-DSK@users.noreply.github.com>
2025-02-01 10:51:20 +0800

    Implement relic SubStat

---
+++ src/main/java/org/example/relic/SubStatType.java
@@ -0,0 +1,16 @@
+package org.example.relic;
+
+public enum SubStatType {
+    HP_FLAT,
+    ATK_FLAT,
+    DEF_FLAT,
+    HP_RATIO,
+    ATK_RATIO,
+    DEF_RATIO,
+    SPD_FLAT,
+    CRIT_RATE,
+    CRIT_DMG,
+    EFFECT,
+    EFF_RES,
+    BREAK
+}
---
+++ src/test/java/SubStatTest.java
@@ -0,0 +1,10 @@
+import org.example.relic.SubStatValueStore;
+import org.junit.jupiter.api.Test;
+
+public class SubStatTest {
+    @Test
+    public void testSubStatValue() {
+        SubStatValueStore subStatValueStore = new SubStatValueStore();
+        System.out.println(subStatValueStore);
+    }
+}
